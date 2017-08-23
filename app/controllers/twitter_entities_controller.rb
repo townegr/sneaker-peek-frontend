1e35971d4391997a604126fb90b4707460a1a104
gtowne <gtowne@rbmtechnologies.com>
2017-08-23 17:59:20 -0400

    Frontend reads from backend microservice

--- app/controllers/twitter_entities_controller.rb
+++ app/controllers/twitter_entities_controller.rb
@@ -2,4 +2,8 @@ class TwitterEntitiesController < ApplicationController
   def index
     @twitter_entities = SneakerPeekInfoService.new.get_twitter_entities_data
   end
+
+  def show
+    @twitter_entity = SneakerPeekInfoService.new.get_twitter_entity_data(params[:id])
+  end
 end
