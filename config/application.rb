df103b614b6a96749e64c472676158a01dc448c0
gtowne <gtowne@rbmtechnologies.com>
2017-08-23 23:01:14 -0400

    Add config file

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,13 @@ Bundler.require(*Rails.groups)
 
 module SneakerPeekAdmin
   class Application < Rails::Application
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
--- lib/sneaker_peek_info_service.rb
+++ lib/sneaker_peek_info_service.rb
@@ -1,10 +1,10 @@
 class SneakerPeekInfoService
-  def get_twitter_entities_data#(current_user)
-    JSON.parse(Http.get("#{backend_sneaker_peek_info_url}/twitter_entities.json?user_name=gtowney").body)['all_followings'].with_indifferent_access
+  def get_twitter_entities_data
+    JSON.parse(Http.get("#{backend_sneaker_peek_info_url}/twitter_entities.json?user_name=#{ENV['GLOBAL_USER']}").body)['all_followings'].with_indifferent_access
   end
 
   def get_twitter_entity_data(uid)
-    JSON.parse(Http.get("#{backend_sneaker_peek_info_url}/twitter_entities/#{uid}.json?user_name=gtowney&service=frontend").body)['tweets'].with_indifferent_access
+    JSON.parse(Http.get("#{backend_sneaker_peek_info_url}/twitter_entities/#{uid}.json?user_name=#{ENV['GLOBAL_USER']}&service=frontend").body)['tweets'].with_indifferent_access
   end
 
   private
